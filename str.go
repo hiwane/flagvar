5eadfec520ced23c46c1330bf93207226f883a37
Hidenao IWANE <hiwane@users.noreply.github.com>
2023-03-10 13:44:27 +0900

    add StrVar

---
+++ str.go
@@ -0,0 +1,26 @@
+package flagvar
+
+type StrVar struct {
+	val      string
+	assigned bool
+}
+
+func NewStrVar(defval string) *StrVar {
+	v := new(StrVar)
+	v.val = defval
+	return v
+}
+
+func (dv *StrVar) Set(opt string) error {
+	dv.assigned = true
+	dv.val = opt
+	return nil
+}
+
+func (dv *StrVar) String() string {
+	return dv.val
+}
+
+func (dv *StrVar) Value() (string, bool) {
+	return dv.val, dv.assigned
+}
