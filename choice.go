f69415f29c103b2d18315b02aab2cc13154615a4
Hidenao IWANE <hiwane@users.noreply.github.com>
2023-04-27 21:18:17 +0900

    add choice.go

---
+++ choice.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ChoiceVar struct {
+	val      string
+	cand     []string
+	assigned bool
+}
+
+func NewChoiceVar(defval string, cand []string) *ChoiceVar {
+	v := new(ChoiceVar)
+	v.val = defval
+	v.cand = cand
+	return v
+}
+
+func (cv *ChoiceVar) isValid(s string) bool {
+	for _, c := range cv.cand {
+		if c == s {
+			return true
+		}
+	}
+	return false
+}
+
+func (cv *ChoiceVar) Set(opt string) error {
+	if !cv.isValid(opt) {
+		return fmt.Errorf("invalid choice: choose from %v", cv.cand)
+	}
+	cv.assigned = true
+	cv.val = opt
+	return nil
+}
+
+func (cv *ChoiceVar) String() string {
+	return cv.val
+}
+
+func (cv *ChoiceVar) Value() (string, bool) {
+	return cv.val, cv.assigned
+}
+
+func (cv *ChoiceVar) IsAssigned() bool {
+	return cv.assigned
+}
